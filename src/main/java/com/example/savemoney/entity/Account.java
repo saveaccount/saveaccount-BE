57b13f9218e2b076e30d7c88cd17ace5e6e348b4
wjddn <wjddngur0334@naver.com>
2024-11-08 17:21:50 +0900

    .

--- src/main/java/com/example/savemoney/entity/Account.java
+++ src/main/java/com/example/savemoney/entity/Account.java
@@ -3,9 +3,7 @@ package com.example.savemoney.entity;
 import com.example.savemoney.enumeration.AccountType;
 import com.example.savemoney.enumeration.BankType;
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -35,9 +33,23 @@ public class Account extends BaseEntity{
     @Column(nullable = false)
     private BankType bank;
 
+    // balance setter 추가
+    @Setter
     @Column(nullable = false)
     private int balance;
 
     @OneToMany(mappedBy = "senderAccount", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Statement> statements=new ArrayList<>();
+
+    // 생성자 추가
+    @Builder
+    public Account(String accountNum, BankType bank, User user, String pw, AccountType type, int balance) {
+        this.account_num = accountNum;
+        this.bank = bank;
+        this.user = user;
+        this.pw = pw;
+        this.type = type;
+        this.balance = balance;
+    }
+
 }
