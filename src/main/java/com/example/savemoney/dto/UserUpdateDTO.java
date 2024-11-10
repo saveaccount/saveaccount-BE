0a8e9d502138cf18704ae5b459316293aabb1e7c
chanu.develop@gmail.com <chanu.develop@gmail.com>
2024-11-10 14:39:13 +0900

    Add: User Update DTO

---
+++ src/main/java/com/example/savemoney/dto/UserUpdateDTO.java
@@ -0,0 +1,20 @@
+package com.example.savemoney.dto;
+
+import com.example.savemoney.enumeration.Gender;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserUpdateDTO {
+
+    private String email;
+    private String password;
+    private String passwordConfirm;
+    private String name;
+    private Gender gender;
+    private int age;
+    private String phone;
+}
