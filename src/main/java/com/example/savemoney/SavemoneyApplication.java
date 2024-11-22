a1e3d40404cc1c021af2c4c9f96df6d9900c507a
wlswnsdn <53937347+wlswnsdn@users.noreply.github.com>
2024-11-23 01:09:41 +0900

    test action really works

--- src/main/java/com/example/savemoney/SavemoneyApplication.java
+++ src/main/java/com/example/savemoney/SavemoneyApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SavemoneyApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SavemoneyApplication.class, args);
+        SpringApplication.run(SavemoneyApplication.class, args!);
     }
 
 }
